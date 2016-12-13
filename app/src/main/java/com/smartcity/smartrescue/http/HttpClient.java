c3da74a2a85cea49a7f9bd52a461b2a822dbbe8f
Pascal Tung <tung.pascal@etu.unice.fr>
2016-12-13 14:30:34 +0100

    refactor httpclient

---
+++ app/src/main/java/com/smartcity/smartrescue/http/HttpClient.java
@@ -0,0 +1,48 @@
+package com.smartcity.smartrescue.http;
+
+import java.io.IOException;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.logging.HttpLoggingInterceptor;
+import timber.log.Timber;
+
+import static com.smartcity.smartrescue.Constants.SERVER_ENDPOINT;
+
+public class HttpClient {
+    public static boolean pushToken(String vehiculeId, String token) {
+        if (vehiculeId.isEmpty() || token.isEmpty()) {
+            throw new IllegalArgumentException("Empty args");
+        }
+
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BASIC);
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .addInterceptor(logging)
+                .build();
+
+        RequestBody body = new FormBody.Builder()
+                .add("vehiculeId", vehiculeId)
+                .add("token", token)
+                .build();
+
+        Request request = new Request.Builder()
+                .url(SERVER_ENDPOINT)
+                .post(body)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            Timber.d("Request token sent. %d", response.code());
+            response.close();
+
+            return true;
+        } catch (IOException e) {
+            Timber.e(e);
+        }
+
+        return false;
+    }
+}
--- app/src/main/java/com/smartcity/smartrescue/settings/PreferenceClickListener.java
+++ app/src/main/java/com/smartcity/smartrescue/settings/PreferenceClickListener.java
@@ -7,18 +7,10 @@ import android.preference.Preference;
 import android.preference.PreferenceManager;
 
 import com.google.firebase.iid.FirebaseInstanceId;
+import com.smartcity.smartrescue.http.HttpClient;
 
-import java.io.IOException;
-
-import okhttp3.FormBody;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
-import okhttp3.logging.HttpLoggingInterceptor;
 import timber.log.Timber;
 
-import static com.smartcity.smartrescue.Constants.SERVER_ENDPOINT;
 import static com.smartcity.smartrescue.settings.SettingsActivity.VEHICULE_ID_KEY;
 
 public class PreferenceClickListener implements Preference.OnPreferenceClickListener {
@@ -41,35 +33,10 @@ public class PreferenceClickListener implements Preference.OnPreferenceClickList
             String token = FirebaseInstanceId.getInstance().getToken();
             Timber.d("Register token %s", token);
 
-            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
-            logging.setLevel(HttpLoggingInterceptor.Level.BASIC);
-            OkHttpClient client = new OkHttpClient().newBuilder()
-                    .addInterceptor(logging)
-                    .build();
-
             SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
             String vehiculeId = sp.getString(VEHICULE_ID_KEY, "");
 
-            RequestBody body = new FormBody.Builder()
-                    .add("vehiculeId", vehiculeId)
-                    .add("token", token)
-                    .build();
-
-            Request request = new Request.Builder()
-                    .url(SERVER_ENDPOINT)
-                    .post(body)
-                    .build();
-            try {
-                Response response = client.newCall(request).execute();
-                Timber.d("Request token sent. %d", response.code());
-                response.close();
-
-                return true;
-            } catch (IOException e) {
-                Timber.e(e);
-            }
-
-            return false;
+            return HttpClient.pushToken(vehiculeId, token);
         }
     }
 }
